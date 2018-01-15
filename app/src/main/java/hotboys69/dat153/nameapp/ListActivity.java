9c146675ff1a06c6d4ac1515b1b6c3b18d22c4d3
Emilracerbil <emilracerbil@gmail.com>
2018-01-15 13:21:05 +0100

    Test Commit

--- app/src/main/java/hotboys69/dat153/nameapp/ListActivity.java
+++ app/src/main/java/hotboys69/dat153/nameapp/ListActivity.java
@@ -2,12 +2,28 @@ package hotboys69.dat153.nameapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ListActivity extends AppCompatActivity {
+
+
+
+    String[] strings = {"1","2","3"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+        ListView la = (ListView) findViewById(R.id.listView);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, strings);
+
+        la.setAdapter(adapter);
+
+
+
     }
 }
