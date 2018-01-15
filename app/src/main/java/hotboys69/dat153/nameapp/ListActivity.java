e0145b2b3507c62767021781958d07669086a01c
GuMMaN <trygve@igesund.com>
2018-01-15 12:37:37 +0100

    xD

---
+++ app/src/main/java/hotboys69/dat153/nameapp/ListActivity.java
@@ -0,0 +1,13 @@
+package hotboys69.dat153.nameapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+    }
+}
--- app/src/main/java/hotboys69/dat153/nameapp/MainActivity.java
+++ app/src/main/java/hotboys69/dat153/nameapp/MainActivity.java
@@ -1,13 +1,30 @@
 package hotboys69.dat153.nameapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        Button button1 = findViewById(R.id.button);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent listPageIntent = new Intent(this, ListActivity.class);
+                startActivity(listPageIntent);
+            }
+        });
     }
 }
