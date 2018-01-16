dc9d5eb44f1ef092a2d94bb580675ad2edf0b8eb
GuMMaN <trygve@igesund.com>
2018-01-16 13:39:32 +0100

    fungerende list names

--- app/src/main/java/hotboys69/dat153/nameapp/ListActivity.java
+++ app/src/main/java/hotboys69/dat153/nameapp/ListActivity.java
@@ -5,20 +5,29 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import java.util.HashMap;
 
 public class ListActivity extends AppCompatActivity {
 
 
 
     String[] strings = {"kunne ikke","laste inn fra","stringarray i xml"};
+    HashMap<String, Integer> pictures;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+        pictures = new HashMap<String, Integer>();
+        pictures.put("Jonas", R.drawable.ekrof);
+        pictures.put("Emil", R.drawable.emilracerbil);
+        pictures.put("Trygve", R.drawable.gumman);
 
         strings = getResources().getStringArray(R.array.names);
 
@@ -31,7 +40,10 @@ public class ListActivity extends AppCompatActivity {
         la.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast toast = Toast.makeText(view.getContext(), strings[i], 50);
+                Toast toast = new Toast(view.getContext());
+                ImageView img = new ImageView(view.getContext());
+                img.setImageResource(pictures.get(strings[i]));
+                toast.setView(img);
                 toast.show();
             }
         });
