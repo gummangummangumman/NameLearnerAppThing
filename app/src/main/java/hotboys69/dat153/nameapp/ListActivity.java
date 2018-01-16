d151b6842c64370a236b8042a4e60cb623d53a97
GuMMaN <trygve@igesund.com>
2018-01-16 13:18:13 +0100

    onClickItem

--- app/src/main/java/hotboys69/dat153/nameapp/ListActivity.java
+++ app/src/main/java/hotboys69/dat153/nameapp/ListActivity.java
@@ -2,8 +2,11 @@ package hotboys69.dat153.nameapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class ListActivity extends AppCompatActivity {
 
@@ -24,5 +27,14 @@ public class ListActivity extends AppCompatActivity {
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, strings);
 
         la.setAdapter(adapter);
+
+        la.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast toast = Toast.makeText(view.getContext(), ""+i, 50);
+                toast.show();
+            }
+        });
+
     }
 }
