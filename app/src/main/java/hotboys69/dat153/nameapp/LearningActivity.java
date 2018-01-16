d954e517946df0675b7698c9f245820c633ceac7
GuMMaN <trygve@igesund.com>
2018-01-16 15:10:50 +0100

    учусь моуд))

--- app/src/main/java/hotboys69/dat153/nameapp/LearningActivity.java
+++ app/src/main/java/hotboys69/dat153/nameapp/LearningActivity.java
@@ -2,6 +2,9 @@ package hotboys69.dat153.nameapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class LearningActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class LearningActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_learning);
+
+        Button guessButton = findViewById(R.id.guessButton);
+        guessButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast toast = Toast.makeText(view.getContext(), "yo this aint implemented", 1000);
+                toast.show();
+            }
+        });
     }
 }
