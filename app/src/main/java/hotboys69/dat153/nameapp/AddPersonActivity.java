6e7d141c270f5dd9a3271af6d2041f6402bf5fe6
ekrof <jonaeker@hotmail.com>
2018-01-17 20:19:25 +0100

    Added imagepicker
    Crashes when choosing picture, close to fixing it though

--- app/src/main/java/hotboys69/dat153/nameapp/AddPersonActivity.java
+++ app/src/main/java/hotboys69/dat153/nameapp/AddPersonActivity.java
@@ -1,13 +1,65 @@
 package hotboys69.dat153.nameapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import java.io.*;
+import java.net.URI;
+import android.graphics.*;
+import 	android.content.Context;
+import android.widget.Toast;
+
 
 public class AddPersonActivity extends AppCompatActivity {
+
+    public Context context = this.getBaseContext();
+
+    public int PICK_IMAGE = 1;
+    //public Context context = this.getBaseContext();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_person);
+
+
+        ImageButton addPictureButton = findViewById(R.id.imageButton);
+        addPictureButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+                i.setType("image/*");
+                startActivityForResult(Intent.createChooser(i, "Select Picture"), PICK_IMAGE);
+            }
+        });
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE) {
+            if (data == null) {
+
+                Toast.makeText(this, "You need to select a picture",
+                        Toast.LENGTH_SHORT).show();
+                return;
+            }
+            InputStream is = null;
+            try {
+                is = context.getContentResolver().openInputStream(data.getData());
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            Bitmap bitmap = BitmapFactory.decodeStream(is);
+
+
+        }
+
+    }
+
 }
+
