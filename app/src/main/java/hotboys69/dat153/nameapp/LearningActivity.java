0b7bed5aa6fad640742f1fcb2a8d8812ea1bcb0a
ChristianPisani <christian96-th@hotmail.com>
2018-01-29 12:49:40 +0100

    Animasjon

--- app/src/main/java/hotboys69/dat153/nameapp/LearningActivity.java
+++ app/src/main/java/hotboys69/dat153/nameapp/LearningActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -70,6 +71,7 @@ public class LearningActivity extends AppCompatActivity {
                     } catch (Exception e) {}
                 }
 
+                img.startAnimation(AnimationUtils.loadAnimation(getBaseContext(), R.anim.spin));
                 img.setImageBitmap(bi);
                 img.setImageURI(p.getPic());
                 et.setText("");
