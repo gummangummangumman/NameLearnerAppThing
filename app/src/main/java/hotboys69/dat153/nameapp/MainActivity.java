ac3f1633d3c78a6817ea6080dbdd35f390b570e9
ekrof <jonaeker@hotmail.com>
2018-01-30 15:53:39 +0100

    Picture internal storage at startup, welcome message etc.

--- app/src/main/java/hotboys69/dat153/nameapp/MainActivity.java
+++ app/src/main/java/hotboys69/dat153/nameapp/MainActivity.java
@@ -3,31 +3,69 @@ package hotboys69.dat153.nameapp;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
+import android.net.Uri;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.support.design.widget.FloatingActionButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    private Uri ownerPicUri = null;
+    public ImageView ownerImg = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
 
         setContentView(R.layout.activity_main);
-        setUpMainMenu();
+        TextView welcomeText = findViewById(R.id.textView3);
+        ownerImg = findViewById(R.id.imageOwner);
 
         SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
         if(!pref.contains("name")) {
             startActivity(new Intent(this, activity_create_owner.class));
-        }
+        }else {
+           String owner =  pref.getString("name",null);
+            welcomeText.append("Welcome "+owner);
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+            try {
+//                FileInputStream fos = openFileInput("owner_image");
+
+                 InputStream is = new BufferedInputStream(new FileInputStream("owner_image"));
+
+                Bitmap myBitmap = BitmapFactory.decodeStream(is);
+                ownerImg.setImageBitmap(myBitmap);
+
+            } catch (IllegalArgumentException e) {
+                e.printStackTrace();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+        setUpMainMenu();
     }
 
     private void setUpMainMenu() {
--- app/src/main/java/hotboys69/dat153/nameapp/activity_create_owner.java
+++ app/src/main/java/hotboys69/dat153/nameapp/activity_create_owner.java
@@ -1,15 +1,35 @@
 package hotboys69.dat153.nameapp;
 
+import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class activity_create_owner extends AppCompatActivity {
+
+    private static final int CAMERA_REQUEST = 1888;
+    public int PICK_IMAGE = 1;
+    private Uri selectedImage = null;
+    public ImageView imageView = null;
+    private FileOutputStream fos = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +37,25 @@ public class activity_create_owner extends AppCompatActivity {
         setContentView(R.layout.activity_create_owner);
 
         Button ownerBtn = findViewById(R.id.set_owner_btn);
+        ImageButton addPictureButton = findViewById(R.id.imageButton);
+        addPictureButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+                i.setType("image/*");
+                startActivityForResult(Intent.createChooser(i, "Select Picture"), PICK_IMAGE);
+            }
+
+        });
+        ImageButton addPictureFromCameraButton = findViewById(R.id.cameraButton);
+        addPictureFromCameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST);
+            }
+        });
+
         final EditText nameText = findViewById(R.id.name);
 
         ownerBtn.setOnClickListener(new View.OnClickListener() {
@@ -28,11 +67,42 @@ public class activity_create_owner extends AppCompatActivity {
                     prefEditor.putString("name", nameText.getText().toString());
                     prefEditor.commit();
 
-                    finish();
+                    String ownerString = selectedImage.toString();
+                    String FILENAME = "owner_image";
+
+                    try {
+                        fos = openFileOutput(FILENAME, Context.MODE_PRIVATE);
+                        fos.write(ownerString.getBytes());
+                        fos.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
                 } else {
                     Toast.makeText(activity_create_owner.this, "Must write name", Toast.LENGTH_SHORT).show();
                 }
+                finish();
+
             }
+
         });
+
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE | requestCode == CAMERA_REQUEST) {
+            if (data == null) {
+                Toast.makeText(this, "You need to select a picture",
+                        Toast.LENGTH_SHORT).show();
+                return;
+            }
+            selectedImage = data.getData();
+            imageView = findViewById(R.id.imageOwner);
+            imageView.setImageURI(selectedImage);
+
+        }
+    }
+
 }
