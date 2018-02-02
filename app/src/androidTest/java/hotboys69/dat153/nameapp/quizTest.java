675ce829ecb4a0eae8cc9c4ea5866e8588efe956
GuMMaN <trygve@igesund.com>
2018-02-02 15:09:34 +0100

    instrumented tests. quiz og add

--- app/src/androidTest/java/hotboys69/dat153/nameapp/quizTest.java
+++ app/src/androidTest/java/hotboys69/dat153/nameapp/quizTest.java
@@ -1,32 +1,79 @@
 package hotboys69.dat153.nameapp;
 
-import android.content.Intent;
+import android.support.test.espresso.Espresso;
 import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.widget.ListView;
 
 import org.junit.*;
+import org.junit.runner.RunWith;
+
 
 import static android.support.test.espresso.Espresso.*;
 import static android.support.test.espresso.action.ViewActions.*;
-import static android.support.test.espresso.assertion.ViewAssertions.*;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.*;
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertTrue;
+
+import static org.hamcrest.CoreMatchers.allOf;
+import static org.hamcrest.CoreMatchers.anything;
 
 /**
  * Created by Jonas on 01.02.2018.
  */
 
+@RunWith(AndroidJUnit4.class)
 public class quizTest {
 
 
     @Rule
-    public ActivityTestRule<LearningActivity> mActivityRule =
+    public ActivityTestRule<LearningActivity> quizActivityRule =
             new ActivityTestRule(LearningActivity.class);
 
 
-    @Test
-    public void openQuiz(){
-        onView(withId(R.layout.activity_learning)).perform(click()).check(matches(isDisplayed()));
+
+    @Before
+    public void setup()
+    {
 
     }
 
+    @After
+    public void cleanup()
+    {
+
+    }
+
+
+    @Test
+    public void scoreIsSameWhenGuessingWrong(){
+        int scoreBeforeClick = quizActivityRule.getActivity().score;
+        onView(withId(R.id.guessButton)).perform(click());
+        assertEquals(quizActivityRule.getActivity().score, scoreBeforeClick);
+    }
+
+    @Test
+    public void scoreIncreases()
+    {
+        int scoreBeforeClick = quizActivityRule.getActivity().score;
+        Person p = quizActivityRule.getActivity().p;
+        onView(withId(R.id.nameGuess)).perform(typeText(p.getName()));
+        Espresso.closeSoftKeyboard();
+        onView(withId(R.id.guessButton)).perform(click());
+        assertEquals(quizActivityRule.getActivity().score, scoreBeforeClick+1);
+    }
+
+    @Test
+    public void ignoresCase()
+    {
+        int scoreBeforeClick = quizActivityRule.getActivity().score;
+        Person p = quizActivityRule.getActivity().p;
+        onView(withId(R.id.nameGuess)).perform(typeText(p.getName().toUpperCase()));
+        Espresso.closeSoftKeyboard();
+        onView(withId(R.id.guessButton)).perform(click());
+        assertEquals(quizActivityRule.getActivity().score, scoreBeforeClick+1);
+    }
 
 }
